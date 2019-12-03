6d4394b63d4d1b0feacf28e6e20f7e3ea9993766
James-Livesey <jamesliveg@gmail.com>
2019-12-03 18:06:16 +0000

    Modify options for YT player

--- play/watch.js
+++ play/watch.js
@@ -4,11 +4,26 @@ var player;
 
 function onYouTubeIframeAPIReady() {
     player = new YT.Player("player", {
+        videoId: "Bey4XXJAqS8",
         width: $("section").width(),
         height: $("section").width() * RATIO_16BY9,
-        videoId: "dQw4w9WgXcQ"
+        playerVars: {
+            autoplay: 1,
+            controls: 0,
+            modestbranding: 1,
+            rel: 0,
+            showinfo: 0,
+            disablekb: 1
+        },
+        events: {
+            onReady: onPlayerReady
+        }
     });
+
+    $("iframe").attr("tabindex", "-1");
 }
+
+function onPlayerReady() {}
 
 $(window).resize(function() {
     player.setSize($("section").width(), $("section").width() * RATIO_16BY9);
