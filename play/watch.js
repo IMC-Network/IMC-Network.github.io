b771aa6efe1fbb09ed3f4710d5583bc99a35ca68
James-Livesey <jamesliveg@gmail.com>
2020-06-17 20:26:34 +0100

    Add sample video for watch page

--- play/watch.js
+++ play/watch.js
@@ -2,28 +2,39 @@ const RATIO_16BY9 = 9 / 16;
 
 var player;
 
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player("player", {
-        videoId: "9oPLm0pRW4w",
-        width: $("section").width(),
-        height: $("section").width() * RATIO_16BY9,
-        playerVars: {
-            autoplay: 1,
-            controls: 1,
-            modestbranding: 1,
-            rel: 0,
-            showinfo: 0
-        },
-        events: {
-            onReady: onPlayerReady
-        }
+$(function() {
+    videojs("player", {}, function() {
+        player = this;
+
+        player.ads();
+
+        player.src("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
+        player.poster("https://upload.wikimedia.org/wikipedia/commons/c/ca/Bbb-splash.png");
+
+        player.on("readyforpreroll", function() {
+            player.ads.startLinearAdMode();
+
+            player.src("https://imcnetwork.cf/media/animations/IMC Play Opening Video.mp4");
+        });
+
+        player.one("adplaying", function() {
+            player.trigger("ads-ad-started");
+        });
+
+        player.one("adended", function() {
+            player.ads.endLinearAdMode();
+        });
+
+        player.trigger("adsready");
+
+        player.width($("section").width());
+        player.height($("section").width() * RATIO_16BY9);
     });
-
-    $("iframe").attr("tabindex", "-1");
-}
-
-function onPlayerReady() {}
+});
 
 $(window).resize(function() {
-    player.setSize($("section").width(), $("section").width() * RATIO_16BY9);
+    if (player) {
+        player.width($("section").width());
+        player.height($("section").width() * RATIO_16BY9);
+    }
 });
